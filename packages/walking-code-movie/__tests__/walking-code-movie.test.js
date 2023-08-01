29998670ae5445b32554896b2383cf7694c7612d
zhangzhuang15 <2592004894@qq.com>
2023-08-01 17:19:21 +0800

    init walking-code-movie

---
+++ packages/walking-code-movie/__tests__/walking-code-movie.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const walkingCodeMovie = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(walkingCodeMovie(), 'Hello from walkingCodeMovie');
+console.info('walkingCodeMovie tests passed');
